a346ea1668f1a4621b6019aa271d1dd45c7ec6fb
Giuliano Peccetto <31479677+PecceG2@users.noreply.github.com>
2023-12-28 20:00:28 -0300

    * Fix: API DolarSI devuelve un valor no documentado
    La API de DolarSi devuelve un objeto con error en algunas solicitudes, cuando debería devolver un string en dicha ubicación según los docs.

--- config-page.php
+++ config-page.php
@@ -24,7 +24,7 @@ function settings_page() {
 				<td>
 					<select name="api_dolar">
 						<option value="0" <?php is_selected(0, 'api_dolar'); ?>>DolarSI</option>
-						<option value="1" <?php is_selected(1, 'api_dolar'); ?>>Estadísticas BCRA</option>
+						<option value="1" <?php is_selected(1, 'api_dolar'); ?>>EstadÃ­sticas BCRA</option>
 						<option value="2" <?php is_selected(2, 'api_dolar'); ?>>BNA (Inestable)</option>
 						<option value="3" <?php is_selected(3, 'api_dolar'); ?>>XE Currency Data API (Premium)</option>
 						<option value="4" <?php is_selected(4, 'api_dolar'); ?>>Valor Manual</option>
@@ -33,35 +33,35 @@ function settings_page() {
 			</tr>
 			
 			<tr valign="top">
-				<th scope="row" style="width:300px;">Configuración de la moneda:</th>
+				<th scope="row" style="width:300px;">ConfiguraciÃ³n de la moneda:</th>
 				<td>
 				<?php
 					switch(get_option('api_dolar')){
 						case 0:
 						//DolarSI Options
 						echo('<select name="dolar_type">');
-						$DSIData = currencyUpdate_DolarSI();
+						$DSIData = (array) currencyUpdate_DolarSI();
 						foreach($DSIData as $key=>$value){
-							echo('<option '.is_selected($key, "dolar_type", false).' value="'.$key.'">'.$value->casa->nombre.' (Compra: '.$value->casa->compra.') (Venta: '.$value->casa->venta.')</option>');
+							echo('<option '.is_selected($key, "dolar_type", false).' value="'.$key.'">'.(is_string($value->casa->nombre) ? $value->casa->nombre : "Otro").' (Compra: '.(is_string($value->casa->compra) ? $value->casa->compra : "-").') (Venta: '.(is_string($value->casa->venta) ? $value->casa->venta : "-").')</option>');
 						}
 						echo("</select>");
 						break;
 						default:
-							echo("<h2>Seleccione un Dolar API y guarda la configuración para mostrar estas opciones.</h2>");
+							echo("<h2>Seleccione un Dolar API y guarda la configuraciÃ³n para mostrar estas opciones.</h2>");
 						break;
 					}
 						
 				?>
 				<select name="dolar_type_cv">
-					<option <?php is_selected('dolarCompra', 'dolar_type_cv');?> value="dolarCompra">Dólar Valor de compra</option>
-					<option <?php is_selected('dolarVenta', 'dolar_type_cv');?> value="dolarVenta">Dólar Valor de venta</option>
-					<option <?php is_selected('dolarAgencia', 'dolar_type_cv');?> value="dolarAgencia">Dólar Valor de agencia</option>
+					<option <?php is_selected('dolarCompra', 'dolar_type_cv');?> value="dolarCompra">DÃ³lar Valor de compra</option>
+					<option <?php is_selected('dolarVenta', 'dolar_type_cv');?> value="dolarVenta">DÃ³lar Valor de venta</option>
+					<option <?php is_selected('dolarAgencia', 'dolar_type_cv');?> value="dolarAgencia">DÃ³lar Valor de agencia</option>
 				</select>
 				</td>
 			</tr>
 			 
 			<tr valign="top">
-				<th scope="row" style="width:300px;">Periodo de actualización de la moneda (en minutos):</th>
+				<th scope="row" style="width:300px;">Periodo de actualizaciÃ³n de la moneda (en minutos):</th>
 				<td>
 					<select name="update_time">
 					<?php
@@ -76,7 +76,7 @@ function settings_page() {
 			</tr>
 
 			<tr valign="top">
-				<th scope="row" style="width:300px;">Valor manual de dólar (Debe estar marcada la opción API Manual):</th>
+				<th scope="row" style="width:300px;">Valor manual de dÃ³lar (Debe estar marcada la opciÃ³n API Manual):</th>
 				<td>
 					<input type="text" name="valor_dolar_manual" value="<?=get_option('dolar_value')?>">
 				</td>
@@ -89,7 +89,7 @@ function settings_page() {
 		<br>
 		<hr>
 		<h2>Modo de uso:</h2>
-		<h3>Ingrese el siguiente código donde desea que se imprima el valor convertido: </h3><h4 style="color: #2cbe68;">[wp-dolarizate valor='INGRESE AQUÍ EL VALOR A CONVERTIR']</h4>
+		<h3>Ingrese el siguiente cÃ³digo donde desea que se imprima el valor convertido: </h3><h4 style="color: #2cbe68;">[wp-dolarizate valor='INGRESE AQUÃ EL VALOR A CONVERTIR']</h4>
 		
 		<input type="hidden" name="action" value="wpd_update_config">
 		<?php submit_button('Guardar cambios'); ?>
